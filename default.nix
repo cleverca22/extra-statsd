390bc7ac513327e21e66061c6c2b2cb86432d3d1
Michael Bishop <cleverca22@gmail.com>
2018-06-12 18:34:23 -0300

    add a static variant

--- default.nix
+++ default.nix
@@ -4,5 +4,8 @@ let
     datadog = pkgs.haskell.lib.dontCheck super.datadog;
   };
   hspkgs = pkgs.haskellPackages.override { inherit overrides; };
-in 
-  hspkgs.callCabal2nix "extra-statsd" ./. {}
+  pkg = hspkgs.callCabal2nix "extra-statsd" ./. {};
+in {
+  normal = pkg;
+  static = pkgs.haskell.lib.justStaticExecutables pkg;
+}
